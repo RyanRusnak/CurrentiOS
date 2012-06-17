b4a8f2562a7d7dc822e14c0daed7f1008467e476
Ryan Rusnak <ryandrusnak@gmail.com>
2012-06-17 15:07:33 -0400

    Initial Commit

---
+++ Current4/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Current4
+//
+//  Created by Ryan Rusnak on 6/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ Current4/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  Current4
+//
+//  Created by Ryan Rusnak on 6/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
